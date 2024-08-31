4a3c4bdcd74a0d0c1230b5b55e188897ae78ea78
JoannePyo <JOANNE.PYO95@qmail.cuny.edu>
2024-08-30 21:01:08 -0400

    updated mongoDB

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv'; // dotenv 모듈 가져오기
 import ImageKit from "imagekit"; // ImageKit 모듈 가져오기
 import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config(); // .env 파일의 환경 변수 로드
 
@@ -17,6 +18,17 @@ app.use(
 
 app.use(express.json());
 
+// Mongoose
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
 const imagekit = new ImageKit({
     urlEndpoint: process.env.IMAGE_KIT_ENDPOINT, // ImageKit URL 엔드포인트
     publicKey: process.env.IMAGE_KIT_PUBLIC_KEY, // ImageKit 공개 키
@@ -29,7 +41,15 @@ app.get("/api/upload", (req, res) => {
     res.send(result); // 클라이언트에 결과 전송
 });
 
+//
+app.post("/api/chats", (req, res) => {
+    const {text} = req.body
+    console.log(text)
+});
+
+
 // 지정된 포트에서 서버 시작
 app.listen(port, () => {
+    connect() // 잘 연결되면 "connected to MongoDB" 메세지 나옴.
     console.log(`Server running on ${port}`); // 서버 실행 중 메시지 출력
 });
